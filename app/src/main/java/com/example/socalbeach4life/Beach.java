c5c9c3f79c61fdfa9fcafa3c567ed76f59ae1419
dennis <dwang984@usc.edu>
2022-10-31 18:57:56 -0700

    added pins to beaches

---
+++ app/src/main/java/com/example/socalbeach4life/Beach.java
@@ -0,0 +1,31 @@
+package com.example.socalbeach4life;
+
+public class Beach {
+
+    private String hours, name;
+    private double latitude, longitude;
+
+    public Beach(String name, String hours, double latitude, double longitude){
+        this.hours = hours;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+
+    public double getLat(){
+        return latitude;
+    }
+
+    public double getLong(){
+        return longitude;
+    }
+
+    public String getHours(){
+        return hours;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
--- app/src/main/java/com/example/socalbeach4life/MapsActivity.java
+++ app/src/main/java/com/example/socalbeach4life/MapsActivity.java
@@ -1,21 +1,85 @@
 package com.example.socalbeach4life;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.socalbeach4life.databinding.ActivityMapsBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private final FirebaseFirestore db;
+
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+
+    private ArrayList<Beach> allBeaches;
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private Location currentLocation;
+
+    public MapsActivity() {
+        this.db = FirebaseFirestore.getInstance();
+        allBeaches = new ArrayList<>();
+    }
+
+    public void getMapsFromDatabase() {
+
+        db.collection("beaches")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Map<String, Object> d = document.getData();
+                                Beach b = new Beach((String) d.get("name"), (String) d.get("hours"), (Double) d.get("latitude"), (Double) d.get("longitude"));
+                                allBeaches.add(b);
+                            }
+                        }
+
+                        pinBeaches();
+                    }
+                });
+    }
+
+    public void pinBeaches() {
+        for (Beach b : allBeaches) {
+            LatLng coords = new LatLng(b.getLat(), b.getLong());
+            mMap.addMarker(new MarkerOptions().position(coords).title(b.getName()));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,29 +87,54 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mapFragment.getMapAsync(MapsActivity.this);
+
+        fetchLocation();
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        getMapsFromDatabase();
     }
+
+
+
+    public void fetchLocation(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            return;
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            currentLocation = location;
+                        }
+                    }
+                });
+        if(currentLocation != null) {
+            LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 101:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchLocation();
+                }
+                break;
+        }
+    }
+
 }
