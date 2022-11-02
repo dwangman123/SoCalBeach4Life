a0b2ff8011bbb58d979fa572f307a14ae068b7c1
Vihan Raj <vihanraj@usc.edu>
2022-11-01 21:58:48 -0700

    Added FE for login and register

---
+++ app/src/main/java/com/example/socalbeach4life/StartActivity.java
@@ -0,0 +1,13 @@
+package com.example.socalbeach4life;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
