8fe58d8dcba6c21fffa007d2221846f794f2c877
Vihan Raj <vihanraj@usc.edu>
2022-11-21 17:27:43 -0800

    added dependcies

---
+++ app/src/test/java/com/example/socalbeach4life/LoginUnitTests.java
@@ -0,0 +1,16 @@
+package com.example.socalbeach4life;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
+import org.robolectric.annotation.LooperMode;
+import static org.junit.Assert.*;
+import static org.robolectric.annotation.LooperMode.Mode.PAUSED;
+
+@RunWith(RobolectricTestRunner.class)
+@RunWith(MockitoJUnitRunner.class)
+@LooperMode(PAUSED)
+public class LoginUnitTests {
+}
