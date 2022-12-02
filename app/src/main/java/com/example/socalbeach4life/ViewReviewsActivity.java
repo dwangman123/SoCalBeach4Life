4ca14daef3265ce70e0d8c56bd37b707de6cd4bc
Vihan Raj <vihanraj@usc.edu>
2022-12-01 16:43:42 -0800

    add view image button

--- app/src/main/java/com/example/socalbeach4life/ViewReviewsActivity.java
+++ app/src/main/java/com/example/socalbeach4life/ViewReviewsActivity.java
@@ -3,21 +3,32 @@ package com.example.socalbeach4life;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -28,6 +39,7 @@ public class ViewReviewsActivity extends AppCompatActivity {
     private FirebaseFirestore db;
     private boolean testing = false;
     public int testReviewCount;
+    private FirebaseStorage storage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +52,7 @@ public class ViewReviewsActivity extends AppCompatActivity {
         this.testing = intent.getBooleanExtra("testing", false);
         if (!testing && id != null) {
             this.db = FirebaseFirestore.getInstance();
+            this.storage = FirebaseStorage.getInstance();
             TextView nameView = (TextView) findViewById(R.id.beachNameView);
             nameView.setText(name);
 
@@ -61,6 +74,31 @@ public class ViewReviewsActivity extends AppCompatActivity {
             testReviewCount = 0;
         }
 
+    }
+
+    public void displayReviewImage(String reviewId) {
+        Dialog builder = new Dialog(this);
+        builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        builder.getWindow().setBackgroundDrawable(
+                new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialogInterface) {
+                //nothing;
+            }
+        });
+
+        ImageView imageView = new ImageView(this);
+
+        StorageReference storageRef = this.storage.getReference();
+        StorageReference reviewImageRef = storageRef.child("images/"+ reviewId);
+
+        Glide.with(this).load(reviewImageRef).into(imageView);
+
+        builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        builder.show();
     }
 
     private void fillScreen(ReviewWrapper wrapper) {
@@ -106,6 +144,18 @@ public class ViewReviewsActivity extends AppCompatActivity {
             parent.addView(ratingBar);
 
             parent.setBackgroundResource(R.drawable.review_box_border);
+            Button viewImageButton = new Button(this);
+            LinearLayout.LayoutParams viewButtonParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            viewImageButton.setLayoutParams(viewButtonParams);
+            viewImageButton.setText("View Image");
+            viewImageButton.setTextSize(15);
+            viewImageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    displayReviewImage(tempReview.getReviewId());
+                }
+            });
+            parent.addView(viewImageButton);
 
             if (Objects.equals(tempReview.getUserId(), this.id)) {
                 Button deleteButton = new Button(this);
